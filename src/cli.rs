59d90d266848f9e90d072c2d965e52645d2b8e13
Eugene Lebedev <eugene.0x90@gmail.com>
2023-03-31 10:02:35 +0300

    Move CLI related code into separate module

---
+++ src/cli.rs
@@ -0,0 +1,85 @@
+use std::env;
+use std::path::{Path, PathBuf};
+use clap::{Arg, ArgMatches, Command, value_parser};
+use crate::logging::get_logging_config;
+
+const USER_HOME_VAR: &'static str = "HOME";
+
+pub const LOG_LEVEL_ARGUMENT: &str = "log-level";
+pub const LOG_LEVEL_DEFAULT_VALUE: &str = "off";
+
+pub const SHOW_KEYS_COMMAND: &str = "show-keys";
+
+pub const OLDER_THAN_DAYS_OPTION: &str = "older-than-days";
+pub const OLDER_THAN_DAYS_DEFAULT_VALUE: usize = 31;
+
+pub const AUTH_LOG_PATH_OPTION: &str = "auth-log-path";
+pub const DEFAULT_AUTH_LOG_PATH: &str = "/var/log";
+
+pub const FILE_OPTION: &str = "file-path";
+
+const VERSION: &str = "0.2.0";
+
+pub fn get_cli_app() -> ArgMatches {
+    Command::new("akt")
+        .about("Authorized Keys Tool for SSH")
+        .version(VERSION)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+
+        .arg(
+            Arg::new(LOG_LEVEL_ARGUMENT)
+                .help("set logging level. possible values: debug, info, error, warn, trace")
+                .long(LOG_LEVEL_ARGUMENT)
+                .default_value(LOG_LEVEL_DEFAULT_VALUE)
+        )
+
+        .subcommand(
+            Command::new(SHOW_KEYS_COMMAND)
+                .about("Show keys which used older than days")
+                .arg(
+                    Arg::new(OLDER_THAN_DAYS_OPTION)
+                        .help("set days")
+                        .value_parser(value_parser!(usize))
+                        .long(OLDER_THAN_DAYS_OPTION)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new(AUTH_LOG_PATH_OPTION)
+                        .help("set path to auth logs")
+                        .value_parser(value_parser!(PathBuf))
+                        .long(AUTH_LOG_PATH_OPTION)
+                        .default_value(DEFAULT_AUTH_LOG_PATH)
+                        .required(false)
+                )
+                .arg(
+                    Arg::new(FILE_OPTION)
+                        .help("set path to authorized_keys file")
+                        .value_parser(value_parser!(PathBuf))
+                        .long(FILE_OPTION)
+                        .required(false)
+                )
+        )
+
+        .get_matches()
+}
+
+pub fn init_logging(matches: &ArgMatches) {
+    let log_level: &str;
+
+    match matches.get_one::<String>(LOG_LEVEL_ARGUMENT) {
+        Some(log_level_arg) => log_level = log_level_arg,
+        None => log_level = LOG_LEVEL_DEFAULT_VALUE
+    }
+
+    let logging_config = get_logging_config(log_level);
+    log4rs::init_config(logging_config).expect("unable to init logging module");
+}
+
+pub fn get_default_authorized_keys_file_path() -> PathBuf {
+    let home_var = env::var_os(USER_HOME_VAR)
+        .expect(&format!("unexpected error: ${USER_HOME_VAR} variable isn't defined"));
+    let home_var_str = home_var.into_string().expect(&format!("unsupported value in ${USER_HOME_VAR} variable"));
+
+    Path::new(&home_var_str).join(".ssh").join("authorized_keys")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::env;
-use std::path::Path;
 use std::path::PathBuf;
 use std::process::exit;
 use authorized_keys::authorizedkeys::{get_authorized_keys_from_file};
-use clap::{Arg, ArgMatches, Command, value_parser};
 use log::info;
 use ssh_auth_log::provider::AuthLogFileProvider;
-use crate::logging::get_logging_config;
+use crate::cli::{AUTH_LOG_PATH_OPTION, DEFAULT_AUTH_LOG_PATH, FILE_OPTION, get_cli_app, get_default_authorized_keys_file_path, init_logging, OLDER_THAN_DAYS_DEFAULT_VALUE, OLDER_THAN_DAYS_OPTION};
 use crate::usecases::oldkeys::get_keys_older_than;
 
+mod cli;
 
 mod usecases;
 mod logging;
@@ -16,71 +14,10 @@ mod logging;
 #[cfg(test)]
 mod tests_common;
 
-#[cfg(windows)]
-const USER_HOME_VAR: &'static str = "USERPROFILE";
-
-#[cfg(not(windows))]
-const USER_HOME_VAR: &'static str = "HOME";
-
-pub const LOG_LEVEL_ARGUMENT: &str = "log-level";
-pub const LOG_LEVEL_DEFAULT_VALUE: &str = "off";
-
-const SHOW_KEYS_COMMAND: &str = "show-keys";
-
-const OLDER_THAN_DAYS_OPTION: &str = "older-than-days";
-const OLDER_THAN_DAYS_DEFAULT_VALUE: usize = 31;
-
-const AUTH_LOG_PATH_OPTION: &str = "auth-log-path";
-const DEFAULT_AUTH_LOG_PATH: &str = "/var/log";
-
-const FILE_OPTION: &str = "file-path";
-
 const EXIT_CODE_ERROR: i32 = 1;
 
-const VERSION: &str = "0.2.0";
-
 fn main() {
-    let matches = Command::new("akt")
-        .about("Authorized Keys Tool for SSH")
-        .version(VERSION)
-        .subcommand_required(true)
-        .arg_required_else_help(true)
-
-        .arg(
-            Arg::new(LOG_LEVEL_ARGUMENT)
-                .help("set logging level. possible values: debug, info, error, warn, trace")
-                .long(LOG_LEVEL_ARGUMENT)
-                .default_value(LOG_LEVEL_DEFAULT_VALUE)
-        )
-
-        .subcommand(
-            Command::new(SHOW_KEYS_COMMAND)
-                .about("Show keys which used older than days")
-                .arg(
-                    Arg::new(OLDER_THAN_DAYS_OPTION)
-                        .help("set days")
-                        .value_parser(value_parser!(usize))
-                        .long(OLDER_THAN_DAYS_OPTION)
-                        .required(false)
-                )
-                .arg(
-                    Arg::new(AUTH_LOG_PATH_OPTION)
-                        .help("set path to auth logs")
-                        .value_parser(value_parser!(PathBuf))
-                        .long(AUTH_LOG_PATH_OPTION)
-                        .default_value(DEFAULT_AUTH_LOG_PATH)
-                        .required(false)
-                )
-                .arg(
-                    Arg::new(FILE_OPTION)
-                        .help("set path to authorized_keys file")
-                        .value_parser(value_parser!(PathBuf))
-                        .long(FILE_OPTION)
-                        .required(false)
-                )
-        )
-
-        .get_matches();
+    let matches = get_cli_app();
 
     init_logging(&matches);
 
@@ -150,22 +87,4 @@ fn main() {
     }
 }
 
-fn init_logging(matches: &ArgMatches) {
-    let log_level: &str;
 
-    match matches.get_one::<String>(LOG_LEVEL_ARGUMENT) {
-        Some(log_level_arg) => log_level = log_level_arg,
-        None => log_level = LOG_LEVEL_DEFAULT_VALUE
-    }
-
-    let logging_config = get_logging_config(log_level);
-    log4rs::init_config(logging_config).expect("unable to init logging module");
-}
-
-fn get_default_authorized_keys_file_path() -> PathBuf {
-    let home_var = env::var_os(USER_HOME_VAR)
-        .expect(&format!("unexpected error: ${USER_HOME_VAR} variable isn't defined"));
-    let home_var_str = home_var.into_string().expect(&format!("unsupported value in ${USER_HOME_VAR} variable"));
-
-    Path::new(&home_var_str).join(".ssh").join("../proxy-user-tests/authorized_keys")
-}
